da795c424b91565b51fc02bfeb514320c2be6341
altaafali092 <altaafali092@gmail.com>
2025-05-05 22:55:53 +0545

    feat(RoleResource): add authorization check for role editing
    Prevent unauthorized access to the role editing page by implementing an `authorizeAccess` method. If the user lacks the 'edit role' permission, a notification is displayed, and they are redirected to the admin page.

--- app/Filament/Resources/RoleResource/Pages/EditRole.php
+++ app/Filament/Resources/RoleResource/Pages/EditRole.php
@@ -4,7 +4,9 @@ namespace App\Filament\Resources\RoleResource\Pages;
 
 use App\Filament\Resources\RoleResource;
 use Filament\Actions;
+use Filament\Notifications\Notification;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Contracts\Auth\Authenticatable;
 
 class EditRole extends EditRecord
 {
@@ -20,5 +22,18 @@ class EditRole extends EditRecord
     protected function getRedirectUrl(): string
     {
         return $this->getResource()::getUrl('index');
+    }
+
+    public function authorizeAccess(): void
+    {
+        if (! auth()->user()->can('edit role')) {
+            Notification::make()
+                ->title('Unauthorized')
+                ->body('You are not allowed to access this page.')
+                ->danger()
+                ->send();
+
+            $this->redirect('/admin'); // Or wherever appropriate
+        }
     }
 }

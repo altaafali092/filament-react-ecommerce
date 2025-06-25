6464555e382c6ccf89ccc48202e49d683d83290a
altaafali092 <altaafali092@gmail.com>
2025-06-25 18:58:47 +0545

    vht

--- resources/js/components/Forontend/Order/Detail.tsx
+++ resources/js/components/Forontend/Order/Detail.tsx
@@ -68,12 +68,10 @@ export default function OrderDetailPage() {
           <div ref={printRef}>
             <InvoiceContent order={order} officeSettings={officeSettings} />
           </div>
-
           <div className="flex justify-end mt-4">
             <Button onClick={handlePrint}><Printer className="w-4 h-4 mr-2" /> Download</Button>
           </div>
         </DialogContent>
-        
       </Dialog>
     </div>
   );
